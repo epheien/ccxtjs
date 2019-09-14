f58ee125f7c34e18b7ff5ed82c9d63fe5beb5eb0
eph <eph@EphMacBook.local>
2019-09-14 16:12:45 +0800

    功能基本测试过了

--- server.js
+++ server.js
@@ -1,33 +1,232 @@
 const Koa = require('koa')
 const ccxt = require('ccxt')
+const bodyParser = require('koa-bodyparser')
+const _ = require('lodash')
 
 const app = new Koa()
 // msleep 函数
 let msleep = (ms) => new Promise(resolve => setTimeout (resolve, ms))
 
-var log = console.log
-var host = '0.0.0.0'
-var port = 3000
+let log = console.log
+let host = '0.0.0.0'
+let port = 3000
 
-var i = 0
+// {
+//   'binance': {'BTC/USDT': exchange}
+//   ...
+// }
+g_ccxt_exchanges = {}
 
-const main = async function(ctx) {
-  let body = ctx.request.body
-  if (ctx.request.path !== '/') {
-    ctx.response.type = 'text'
-    let name = ctx.request.path.substr(1)
-    let result = {}
-    try {
-    } catch (err) {
-    }
+g_exchange_name_dict = {
+  'huobi': 'huobipro',
+  'hitbtc': 'hitbtc2',
+}
 
-    ctx.response.body = JSON.stringify(result)
+function norm_name(name) {
+  let n = _.toLower(name)
+  return _.get(g_exchange_name_dict, n, n)
+}
+
+function norm_symbol(symbol) {
+  let s = _.replace(symbol, '_', '/')
+  return _.toUpper(s)
+}
+
+function split_symbol(symbol) {
+  let s = norm_symbol(symbol)
+  let li = _.split(s, '/')
+  if (li.length < 2) {
+    return [li[0], li[0]]
   } else {
-    ctx.response.body = 'Hello World'
+    return _.slice(li, 0, 2)
   }
 }
 
+async function getCcxtExchange(name, symbol, apiKey, secret,
+                               password='', uid='', reload=false) {
+  name = norm_name(name)
+  symbol = norm_symbol(symbol)
+  let d0 = {}
+  if (g_ccxt_exchanges.hasOwnProperty(name)) {
+    d0 = g_ccxt_exchanges[name]
+  }
+
+  if (reload && d0.hasOwnProperty(symbol)) {
+    await d0[symbol].close()
+    delete d0[symbol]
+  }
+
+  let exchange
+  if (d0.hasOwnProperty(symbol)) {
+    exchange = d0[symbol]
+  } else {
+    try {
+      exchange = eval(`new ccxt.${name}()`)
+    } catch (e) {
+      throw ('Unsupported Exchange: ' + name)
+    }
+  }
+
+  // 初始化
+  //exchange.timeout = 1000 * 55    # milliseconds, default 10000
+  d0[symbol] = exchange
+  exchange.apiKey = apiKey
+  exchange.secret = secret
+  exchange.password = password
+  exchange.uid = uid
+  return exchange
+}
+
+/*
+ * {
+ *    'access_key': '',
+ *    'secret_key': '',
+ *    'passwd_key': '',
+ *    'uid_key': '',
+ *    'nonce': '',
+ *    'method': '',
+ *    'timeout': 10000, # 单位为毫秒
+ *    'params': {
+ *      'symbol': 'ETH_BTC',
+ *      'reload': false,
+ *      'limit': -1,
+ *      'contract_type': 'this_week',
+ *    },
+ * }
+ *
+ * 转化为 ccxt 标准结构
+ * apiKey
+ * secret
+ * password
+ * uid
+ * timeout
+ * reload
+ * symbol
+ * method
+ * # 特定于 method 的参数
+ * params {
+ *  limit
+ * }
+ *
+ */
+function normRequest(data) {
+  // 如果有这个 key, 就直接使用新风格
+  if (data.hasOwnProperty('apiKey')) {
+    return data
+  }
+  return {
+    'apiKey': _.get(data, 'access_key'),
+    'secret': _.get(data, 'secret_key'),
+    'password': _.get(data, 'passwd_key'),
+    'uid': _.get(data, 'uid_key'),
+    'method': _.get(data, 'method'),
+    'timeout': _.get(data, 'timeout', 10.0) * 1000,
+    'params': {
+      'limit': _.get(data, 'params.limit'),
+      'id': _.get(data, 'params.id'),
+      'stmt': _.get(data, 'params.stmt'),
+    }
+  }
+}
+
+async function handleRequest(name, req) {
+  let ex = await getCcxtExchange(name, req.symbol, req.apiKey, req.secret,
+                                 req.password, req.uid, req.reload)
+  log(req)
+  // 重新设置 timeout, 默认 10 秒
+  ex.timeout = _.get(req, 'timeout', 10000)
+  let result = {}
+  let ret
+  try {
+    switch (req.method) {
+      case 'fetchTicker':
+        ret = await ex.fetchTicker(req.symbol)
+        break
+      case 'fetchOrderBook':
+        if (req.params.limit < 0) {
+          ret = await ex.fetchOrderBook(req.symbol)
+        } else {
+          ret = await ex.fetchOrderBook(req.symbol, req.params.limit)
+        }
+        break
+      case 'fetchBalance':
+        ret = await ex.fetchBalance()
+        break
+      case 'fetchOrder':
+        ret = await ex.fetchOrder(req.params.id, req.symbol)
+        break
+      case 'fetchOpenOrders':
+        ret = await ex.fetchOpenOrders(req.symbol)
+        break
+      case 'limitBuy':
+        ret = await ex.createOrder(req.symbol, 'limit', 'buy', req.params.amount, req.params.price)
+        break
+      case 'limitSell':
+        ret = await ex.createOrder(req.symbol, 'limit', 'sell', req.params.amount, req.params.price)
+        break
+      case 'cancelOrder':
+        ret = await ex.cancelOrder(req.params.id, req.symbol)
+        break
+      case 'eval':
+        // NOTE: 务必使用新风格的 req
+        // NOTE: 这个时候, params 直接就是函数的 params
+        log(ex[req.function])
+        ret = await ex[req.function](...req.params)
+        break
+      default:
+        throw new Error('Not Implemented')
+    }
+    result = {
+      'code': 0,
+      //'error': '',
+      'data': ret,
+    }
+  } catch (e) {
+    log(e)
+    result = {
+      'code': 1,
+      'error': e.toString(),
+    }
+  }
+  return result
+}
+
+async function postHandler(ctx) {
+  if (ctx.request.type !== 'application/json') {
+    ctx.throw(400, 'Only accept application/json POST')
+  }
+
+  if (ctx.request.path === '/') {
+    ctx.response.body = 'Hello POST'
+    return
+  }
+
+  // http://localhost:3000/{name}
+  let name = ctx.request.path.substr(1)
+  let body = ctx.request.rawBody
+  let data = JSON.parse(body)
+  let req = normRequest(data)
+  let result = await handleRequest(name, req)
+  log(result)
+  ctx.response.body = JSON.stringify(result)
+}
+
+async function getHandler(ctx) {
+  ctx.response.body = 'Hello GET'
+}
+
+const main = async function(ctx) {
+  if (ctx.request.method === 'GET') {
+    await getHandler(ctx)
+  } else if (ctx.request.method == 'POST') {
+    await postHandler(ctx)
+  }
+}
+
+// FIXME: 无法从 ctx.request.body 获取到 text/plain 的 rawBody
+app.use(bodyParser())
 app.use(main)
-app.listen(port)
-console.log(`======== Running on http://${host}:${port} ========`)
-console.log('(Press CTRL+C to quit)')
+app.listen(port, () => {
+  console.log(`======== Running on http://${host}:${port} ========`)
+  console.log('(Press CTRL+C to quit)')
+})
